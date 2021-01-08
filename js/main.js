f7f0260855d6d8eea48b78047c980ff59d731083
ahamed <sajeeb07ahamed@gmail.com>
2021-01-08 23:10:19 +0600

    Handle the ordering and the children issues

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/**
+ * Tree sortable jQuery library using jQuery UI sortable.
+ *
+ * @package TreeSortable
+ * @license MIT
+ * @author Sajeeb Ahamed
+ */
+
 var $ = jQuery;
 
 const treeSortable = {
@@ -14,19 +22,20 @@ const treeSortable = {
 		return $(treeSortable.options.treeSelector).offset().left;
 	},
 	pxToNumber(str) {
-		return /px$/i.test(str) ? str.slice(0, -2) * 1 : 0;
+		return new RegExp('px$', 'i').test(str) ? str.slice(0, -2) * 1 : 0;
 	},
 	numberToPx(num) {
-		return num + 'px';
+		return `${num}px`;
 	},
 	jQuerySupplements() {
 		const { options } = treeSortable;
 		$.fn.extend({
 			getBranchLevel() {
-				if ($(this).length === 0) return false;
+				if ($(this).length === 0) return 0;
 
 				const { depth } = options;
 				const margin = $(this).css('margin-left');
+
 				return /(px)|(em)|(rem)$/i.test(margin)
 					? Math.floor(margin.slice(0, -2) / depth) + 1
 					: Math.floor(margin / depth) + 1;
@@ -48,9 +57,20 @@ const treeSortable = {
 						.addClass('branch-level-' + newLevel);
 				});
 			},
+			getParent() {
+				const level = $(this).getBranchLevel() || 1;
+				let $prev = $(this).prev('.tree-branch');
+
+				while ($prev.length && $prev.getBranchLevel() >= level) {
+					$prev = $prev.prev('.tree-branch');
+				}
+
+				return $prev;
+			},
 			getChildren() {
 				let $children = $();
-				$(this).each(function () {
+
+				this.each(function () {
 					let level = $(this).getBranchLevel() || 1,
 						$next = $(this).next('.tree-branch');
 
@@ -62,11 +82,35 @@ const treeSortable = {
 
 				return $children;
 			},
-			getUpperBranch() {
+			nextBranch() {
+				return $(this).next();
+			},
+			prevBranch() {
 				return $(this).prev();
 			},
-			getLowerBranch() {
-				return $(this).next().next();
+			nextSibling() {
+				let level = $(this).getBranchLevel() || 1,
+					$next = $(this).next('.tree-branch'),
+					nextLevel = $next.getBranchLevel();
+
+				while ($next.length && nextLevel > level) {
+					$next = $next.next('.tree-branch');
+					nextLevel = $next.getBranchLevel();
+				}
+
+				return nextLevel === level ? $next : $();
+			},
+			prevSibling() {
+				let level = $(this).getBranchLevel() || 1,
+					$prev = $(this).prev('.tree-branch'),
+					prevLevel = $prev.getBranchLevel();
+
+				while ($prev.length && prevLevel > level) {
+					$prev = $prev.prev('.tree-branch');
+					prevLevel = $prev.getBranchLevel();
+				}
+
+				return prevLevel === level ? $prev : $();
 			},
 		});
 	},
@@ -77,18 +121,32 @@ const treeSortable = {
 		/** Store the current level, for sorting the item after stop dragging. */
 		let currentLevel = 1,
 			originalLevel = 1,
-			childrenBus = null;
+			childrenBus = null,
+			helperHeight = 0,
+			changed = false;
 
+		/** Update the placeholder branch level by new level. */
 		const updatePlaceholder = (placeholder, level) => {
 			placeholder.updateBranchLevel(level);
 			currentLevel = level;
+		};
+
+		/** Check if we can swap items vertically for branch with children */
+		const canSwapItems = ui => {
+			let offset = ui.helper.offset(),
+				height = offset.top + helperHeight,
+				nextBranch = ui.placeholder.nextBranch(),
+				nextBranchOffset = nextBranch.offset() || 0,
+				nextBranchHeight = nextBranch.outerHeight();
+
+			return height > nextBranchOffset.top + nextBranchHeight / 3;
 		};
 
 		$(treeSelector).sortable({
 			handle: '.branch-drag-handler',
 			placeholder: 'sortable-placeholder',
 			items: '> *',
-			start(e, ui) {
+			start(_, ui) {
 				/**
 				 * Synchronize the placeholder level with the item's level.
 				 *
@@ -96,12 +154,10 @@ const treeSortable = {
 				const level = ui.item.getBranchLevel();
 				ui.placeholder.updateBranchLevel(level);
 
-				// Store the original level
+				/**  Store the original level. */
 				originalLevel = level;
 
-				/**
-				 * Fill the children bus with the children
-				 */
+				/** Fill the children bus with the children. */
 				childrenBus = ui.item.find('.children-bus');
 				childrenBus.append(ui.item.next().getChildren());
 
@@ -114,62 +170,109 @@ const treeSortable = {
 
 				height += height > 0 ? pxToNumber(placeholderMarginTop) : 0;
 				height += ui.helper.outerHeight();
-
+				helperHeight = height;
 				height -= 2;
 
 				let width =
 					ui.helper.find('.branch-drag-handler').outerWidth() - 2;
 				ui.placeholder.css({ height, width });
 
+				const tmp = ui.placeholder.nextBranch();
+				tmp.css('margin-top', numberToPx(helperHeight));
+				ui.placeholder.detach();
+				$(this).sortable('refresh');
+				ui.item.after(ui.placeholder);
+				tmp.css('margin-top', 0);
+
 				// Set the current level by the initial item's level.
 				currentLevel = level;
 			},
-			change(e, ui) {
-				console.log('change');
-			},
-			sort(e, ui) {
+			sort(_, ui) {
 				const { options, getTreeEdge } = treeSortable;
 				const { depth, maxLevel } = options;
 				let treeEdge = getTreeEdge(),
 					offset = ui.helper.offset(),
-					currentBranchEdge = offset.left;
+					currentBranchEdge = offset.left,
+					lowerBound = 1,
+					upperBound = maxLevel;
 
-				/** Get the upper branch level. */
-				let upperBranchLevel = ui.helper
-					.getUpperBranch()
-					.getBranchLevel();
-				upperBranchLevel += 1;
+				/**
+				 * Calculate the upper bound. The upper bound would be,
+				 * the minimum value between the
+				 * (previous branch level + 1) and the maxLevel.
+				 */
+				let prevBranch = ui.placeholder.prevBranch();
+				prevBranch =
+					prevBranch[0] === ui.item[0]
+						? prevBranch.prevBranch()
+						: prevBranch;
 
-				/** Get the lower branch level. */
-				let lowerBranchLevel = ui.helper
-					.getLowerBranch()
-					.getBranchLevel();
-				lowerBranchLevel -= 1;
+				let prevBranchLevel = prevBranch.getBranchLevel();
+				upperBound = Math.min(prevBranchLevel + 1, maxLevel);
+
+				/**
+				 * Calculate the lower bound. The lower bound would be,
+				 * the maximum value between the
+				 * Next Sibling Level and 1
+				 */
+				let nextSibling = ui.placeholder.nextSibling();
+
+				if (nextSibling.length) {
+					let placeholderLevel = ui.placeholder.getBranchLevel() || 1;
+					lowerBound = Math.max(1, placeholderLevel);
+				}
 
 				/**
 				 * Calculate the position which is the current helper offset left
 				 * minus the tree parent's offset left.
 				 * Find the changed level by dividing the position by depth value.
-				 * The level would be ranged in [1, min(upperBranchLevel, maxLevel)]
+				 *
+				 * The final valid changed level would be a value
+				 * between upper and lower bound inclusive.
 				 */
 				let position = Math.max(0, currentBranchEdge - treeEdge);
 				let newLevel = Math.floor(position / depth) + 1;
-				let lowerBound = Math.max(1, lowerBranchLevel),
-					upperBound = Math.min(upperBranchLevel, maxLevel);
-
-				newLevel = Math.min(upperBound, Math.max(lowerBound, newLevel));
+				newLevel = Math.max(lowerBound, Math.min(newLevel, upperBound));
 
 				/** Update the placeholder position by the changed level. */
 				updatePlaceholder(ui.placeholder, newLevel);
+
+				if (canSwapItems(ui)) {
+					ui.placeholder.nextBranch().after(ui.placeholder);
+					$(this).sortable('refreshPositions');
+				}
 			},
-			stop(e, ui) {
+			change(_, ui) {
+				let prevBranch = ui.placeholder.prevBranch();
+
+				prevBranch =
+					prevBranch[0] === ui.item[0]
+						? prevBranch.prevBranch()
+						: prevBranch;
+
+				/**
+				 * After changing branches bound the placeholder to the
+				 * changed boundary.
+				 */
+				let prevBranchLevel = prevBranch.getBranchLevel() || 1;
+				ui.placeholder.detach();
+				let children = prevBranch.getChildren();
+				if (children && children.length) prevBranchLevel += 1;
+				prevBranch.after(ui.placeholder);
+
+				ui.placeholder.updateBranchLevel(prevBranchLevel);
+			},
+			stop(_, ui) {
+				/**
+				 * Place the children after the sorted item,
+				 * and clear the children bus.
+				 */
 				const children = childrenBus.children().insertAfter(ui.item);
 				childrenBus.empty();
 
 				/** Update the item by currently changed level. */
 				ui.item.updateBranchLevel(currentLevel);
 				children.shiftBranchLevel(currentLevel - originalLevel);
-				console.log(ui.item.getChildren());
 			},
 		});
 	},
